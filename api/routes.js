597b99a680e050fb806913b030a17bd4206d84a0
gokul <gokulchandan18@gmail.com>
2023-02-10 00:49:56 +0530

    first commit

---
+++ api/routes.js
@@ -0,0 +1,15 @@
+
+
+var controller = require('../controller');
+
+module.exports = function(app) {
+
+   app.route('/about')
+
+       .get(controller.about);
+
+   app.route('/distance/:zipcode1/:zipcode2')
+
+       .get(controller.get_distance);
+
+};
